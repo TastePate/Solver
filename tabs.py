661198841347a1385f8a1d19ecdb519be1864fd9
TastePate <pavelarz14@gmail.com>
2023-03-29 18:36:36 +0500

    Исправил построение графиков

--- tabs.py
+++ tabs.py
@@ -28,38 +28,27 @@ class StatisticsTab(ttk.Frame):
         y_data = self.data.values()
 
         self.figure = Figure(figsize=(6, 4), dpi=100)
-        self.figure_canvas = FigureCanvasTkAgg(self.figure, self)
-
-        NavigationToolbar2Tk(self.figure_canvas, self)
+        self.canvas = FigureCanvasTkAgg(self.figure, self)
 
         self.axes = self.figure.add_subplot()
 
         self.axes.bar(x_data, y_data)
+        self.axes.set_yticks(range(0, 1000))
         self.axes.set_title('Статистика правильных ответов')
         self.axes.set_ylabel('Кол-во')
 
-        self.figure_canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-        # self.right_answers_label = Label(self, font=("Times New Roman", 10),
-        #                                  text=f'Правильных ответов: {self.statistics.right_answers}')
-        # self.wrong_answers_label = Label(self, font=("Times New Roman", 10),
-        #                                  text=f'Неправильных ответов: {self.statistics.wrong_answers}')
-        #
-        # self.right_answers_label.pack()
-        # self.wrong_answers_label.pack()
+        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        self.canvas.draw()
 
         self.update()
 
     def update(self):
-        # self.right_answers_label.config(font=("Times New Roman", 10),
-        #                                 text=f'Правильных ответов: {self.statistics.right_answers}')
-        # self.wrong_answers_label.config(font=("Times New Roman", 10),
-        #                                 text=f'Неправильных ответов: {self.statistics.wrong_answers}')
-        self.data['Правильные ответы'] = self.statistics.right_answers
-        self.data['Неправильные ответы'] = self.statistics.wrong_answers
-        self.figure.canvas.draw()
-        self.figure.canvas.flush_events()
-        self.after(100, self.update)
+        self.axes.bar(['Правильные ответы', 'Неправильные ответы'],
+                      [self.statistics.right_answers, self.statistics.wrong_answers],
+                      color='b',
+                      width=0.1)
+        self.canvas.draw()
+        self.after(1000, self.update)
 
 
 class QuestionTab(ttk.Frame):
@@ -91,6 +80,7 @@ class QuestionTab(ttk.Frame):
         
     def accept_answer(self):
         self.accept_button.config(state=DISABLED)
+        self.answer_entry.config(state=DISABLED)
         if self.answer_entry.get() == str(self.question.right_answer):
             self.statistics.right_answers += 1
         else:
@@ -100,7 +90,9 @@ class QuestionTab(ttk.Frame):
 
     def next_question(self):
         self.right_or_not_label.config(text="")
+        self.answer_entry.config(state=NORMAL)
         self.answer_entry.delete(0, END)
+        self.accept_button.config(state=NORMAL)
         self.question = Question(1, 100)
         self.question_label.config(text=self.question.__str__())
 
@@ -108,3 +100,5 @@ class QuestionTab(ttk.Frame):
         self.answer_entry.pack()
         self.accept_button.pack()
         self.right_or_not_label.pack()
+
+
