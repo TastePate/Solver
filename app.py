61b5a8ab30c225a75c780a3b98c2104774ff6aec
TastePate <pavelarz14@gmail.com>
2023-05-15 16:52:49 +0500

    Created user label

--- app.py
+++ app.py
@@ -3,30 +3,36 @@ from tkinter import *
 
 from db_worker import Database
 from statistics import Statistics
-from tabs import StatisticsTab, QuestionTab
+from tabs import StatisticsTab, QuestionTab, LeadersBoard
 
 
 class App(Tk):
 
-    def __init__(self):
+    def __init__(self, user: str):
         super().__init__()
+
+        self.user = user
 
         self.protocol("WM_DELETE_WINDOW", self.destroy)
         self.tab_control = ttk.Notebook(self)
 
         self.db = Database('users.db')
 
-        self.statistics = Statistics()
+        self.statistics = Statistics(self.db.get_value(user)[3], self.db.get_value(user)[4])
+        self.user_label = Label(self, text=f'Текущий пользователь: {user}')
 
         self.question_tab = QuestionTab(self.tab_control, self.statistics)
         self.statistics_tab = StatisticsTab(self.tab_control, self.statistics)
+        self.leaders_tab = LeadersBoard(self.tab_control)
 
         self.tab_control.add(self.question_tab, text="Вопросы")
         self.tab_control.add(self.statistics_tab, text="Статистика")
+        self.tab_control.add(self.leaders_tab, text="Таблица лидеров")
 
         self.tab_control.pack(expand=1, fill="both")
 
         self.question_tab.next_question()
 
     def destroy(self):
-        pass
+        self.db.update_value(self.user, self.statistics)
+        super().destroy()
